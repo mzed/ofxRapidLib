aba97c1a4f6118ae87fefde9c33e7e97aa887660
mzed <github@mikezed.com>
2017-12-04 12:23:43 +0000

    hereis the handwriting example

---
+++ example_handwriting/src/ofApp.cpp
@@ -0,0 +1,108 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(0, 0, 0);
+    instructionCount = 0;
+    instructions[0] = "Draw a letter V with the mouse.";
+    instructions[1] = "Now draw a letter Z.";
+    instructions[2] = "Draw shapes for continuous classification.";
+    
+    tempSeries.label = "V";
+    trained = false;
+    classLabel = "";
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofDrawBitmapString(instructions[instructionCount], 20, 20);
+    ofDrawBitmapString(classLabel, 20, 750);
+    if (instructionCount == 2) {
+        ofDrawBitmapString("Classifying on the last " + std::to_string(classifier.getMinLength()) + " mouse positions.", 600, 750);
+    }
+  
+    line.draw();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    ofPoint pt;
+    pt.set(x,y);
+    line.addVertex(pt);
+    
+    //Training data for machine learning
+    if (instructionCount < 2) {
+        tempSeries.input.push_back({double(x), double(y)});
+    } else {
+        classLabel = "class found: " + classifier.runContinuous({double(x), double(y)});
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    line.clear();
+    if (instructionCount == 1) {
+        //Put the V example in the training set
+        trainingSet.push_back(tempSeries);
+        //Get ready for Z example
+        tempSeries.input.clear();
+        tempSeries.label = "Z";
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    if (instructionCount == 1) {
+        //Push the "Z" example into the set and train
+        trainingSet.push_back(tempSeries);
+        trained = classifier.train(trainingSet);
+    }
+    if (instructionCount < 2) {
+        ++instructionCount;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+    
+}
---
+++ example_handwriting/src/ofApp.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxRapidLib.h"
+
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    //Drawing
+    ofPolyline line;
+    std::string instructions[3];
+    char instructionCount;
+    
+    //RapidLib
+    seriesClassification classifier;
+    std::vector<trainingSeries> trainingSet;
+    trainingSeries tempSeries;
+    bool trained;
+    std::string classLabel;
+    
+};
