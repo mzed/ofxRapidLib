88227911b2db9050e4839b6756ef39285f024f52
Michael Zbyszyński <github@mikezed.com>
2018-07-09 17:18:49 +0100

    working in ofx 0.101.0

--- example-objectClassification/src/ofApp.cpp
+++ example-objectClassification/src/ofApp.cpp
@@ -126,7 +126,7 @@ void ofApp::draw(){
     
     if (recordingState) {
         trainingOutput = { double(recordingState) };
-        trainingExample tempExample;
+        rapidlib::trainingExample tempExample;
         tempExample.input = trainingInput;
         tempExample.output = trainingOutput;
         myData.push_back(tempExample);
